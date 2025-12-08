a6fed7db247655565d62d98578a0cb78b1183c75
tan90xx <592026773@qq.com>
2025-12-08 22:41:01 +0800

    update all

--- libs/FastSpeech2/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model import FastSpeech2
-
-__all__ = ['FastSpeech2']
--- libs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hubert.model import HubertSoft
-from .JDC.model import JDCNet
-
-__all__ = ['HubertSoft', 'JDCNet']
